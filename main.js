7b712038ab35d419abf8639fba85d5631701bff4
Taranveer Singh <taranveersingh93@gmail.com>
2023-04-19 23:22:13 -0400

    Create player based on user input

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 // query selectors
 var userID = document.querySelector("#user-name");
+var userAvatar = document.querySelector("#avatar");
 var formSubmitBtn = document.querySelector(".form-submit-btn")
+
+var computerPlayer = {
+  name: "Computer",
+  avatar: "&#x1F4BB;",
+  wins: 0
+};
+var humanPlayer = {};
 
 // event listeners
 userID.addEventListener("keyup", allowSubmit);
+formSubmitBtn.addEventListener("click", function() {
+  createPlayer(humanPlayer);
+});
 
 // functions 
 function allowSubmit() {
@@ -13,3 +24,14 @@ function allowSubmit() {
     formSubmitBtn.disabled = true;
   }
 }
+
+function createPlayer(playerObject) {
+  var proxyPlayer = {...playerObject};
+  var label = userID.value;
+  var icon = userAvatar.value;
+  proxyPlayer.name = label;
+  proxyPlayer.avatar = icon;
+  playerObject = proxyPlayer;
+  return playerObject;
+}
+
