fe635dce01233592225058a234079a206b3c6497
Taranveer Singh <taranveersingh93@gmail.com>
2023-04-20 01:36:43 -0400

    Remove the button modification

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 // query selectors
 var userID = document.querySelector("#user-name");
 var userAvatar = document.querySelector("#avatar");
-var formSubmitBtn = document.querySelector(".form-submit-btn")
-var formNewSubmitBtn = document.querySelector(".form-new-submit-btn")
+var formSubmitBtn = document.querySelector(".form-submit-btn");
+var formNewSubmitBtn = document.querySelector(".form-new-submit-btn");
+var playerIcon = document.querySelector(".player-icon");
+var playerName = document.querySelector(".player-name");
+var playerScore = document.querySelector(".player-score");
 
+//Global variables
 var computerPlayer = {
   name: "Computer",
   avatar: "&#x1F4BB;",
@@ -14,16 +18,18 @@ var humanPlayer = {};
 // event listeners
 userID.addEventListener("keyup", allowSubmit);
 formSubmitBtn.addEventListener("click", function() {
-  modifySubmitBtn();
-  createPlayer(humanPlayer);
+  humanPlayer = createPlayer(humanPlayer);
+  renderPlayer(humanPlayer, playerIcon, playerName, playerScore);
 });
 
-// functions 
+// Data model functions 
 function allowSubmit() {
   if(userID.value) {
     formSubmitBtn.disabled = false;
+    formSubmitBtn.classList.add("submit-btn-alt");
   } else {
     formSubmitBtn.disabled = true;
+    formSubmitBtn.classList.remove("submit-btn-alt");
   }
 }
 
@@ -38,7 +44,15 @@ function createPlayer(playerObject) {
   var icon = userAvatar.value;
   proxyPlayer.name = label;
   proxyPlayer.avatar = icon;
+  proxyPlayer.wins = 0;
   playerObject = proxyPlayer;
   return playerObject;
 }
 
+//DOM functions
+
+function renderPlayer(playerObject, domIcon, domName, domScore) {
+  domIcon.innerText = playerObject.avatar;
+  domName.innerText = playerObject.name;
+  domScore.innerText = `Wins: ${playerObject.wins}`;
+}
